2c15c70212e7ccb11214616b3ec83e41710d7ce4
Ole Kristian Sunde <ole.sunde@gmail.com>
2022-01-24 20:18:13 +0100

    adds bucket to HomeStack

--- lib/home-stack.ts
+++ lib/home-stack.ts
@@ -1,8 +1,11 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class HomeStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
+
+        new Bucket(this, 'IndexBucket')
     }
 }
--- test/home.test.ts
+++ test/home.test.ts
@@ -1,5 +1,12 @@
-describe('HomeStack', () => {
-    test('Bucket', () => {
+import { Stack } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { HomeStack } from "../lib/home-stack";
 
+describe('HomeStack', () => {
+    test('Bucket Created', () => {
+        const stack = new HomeStack(new Stack(), 'HomeStack')
+
+        const template = Template.fromStack(stack)
+        template.resourceCountIs("AWS::S3::Bucket", 1)
     })
 });
