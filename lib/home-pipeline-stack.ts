d241927b9f3aa9a9331a884d8d21b2e6813ea9f0
Ole Kristian Sunde <ole.sunde@gmail.com>
2022-01-24 19:59:49 +0100

    adds deploy stage (with HomeStack)

--- lib/home-pipeline-stack.ts
+++ lib/home-pipeline-stack.ts
@@ -1,12 +1,13 @@
 import { Stack, StackProps, SecretValue } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
+import { HomePipelineStage } from './home-pipeline-stage'
 
 export class HomePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'HomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('olesu/home', 'main', {
@@ -15,5 +16,8 @@ export class HomePipelineStack extends Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       })
     })
+
+    const deploy = new HomePipelineStage(this, 'Deploy')
+    pipeline.addStage(deploy)
   }
 }
