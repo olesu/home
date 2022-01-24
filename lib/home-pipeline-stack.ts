5b7b6e713ec98e1e6361a7d1e56b24aff7df75ff
Ole Kristian Sunde <ole.sunde@gmail.com>
2022-01-24 19:40:43 +0100

    rename HomeStack -> HomePipelineStack

--- lib/home-pipeline-stack.ts
+++ lib/home-pipeline-stack.ts
@@ -2,12 +2,12 @@ import { Stack, StackProps, SecretValue } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
 
-export class HomeStack extends Stack {
+export class HomePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
     new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'HomepagePipeline',
+      pipelineName: 'HomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('olesu/home', 'main', {
           authentication: SecretValue.secretsManager('github-token-2')
